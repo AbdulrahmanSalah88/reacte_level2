df28a80eca8783f5afb653dc0b0c735d8c8d2b42
Abdulrahman Boda <127202241+AbdulrahmanSalah88@users.noreply.github.com>
2023-03-19 02:28:15 +0200

    lesson3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const [person, setPerson] = useState("Ali Hassan");
+  const [Age, setAge] = useState(28);
+  const [Count, setCount] = useState(0);
+
+  // The Elements of => (ChangeAge)
+  const ChangeAge = () => {
+    setAge(10)
+  }
+
+  // The Elements of => (ChangeAge)
+  const Counter = () => {
+    setCount(Count + 1)
+  }
+
+
   return (
     <div className="App">
-      <button>count is 0</button>
+      <h2>My Name is {person}</h2>
+      <button onClick={() => { setPerson("BodaSalah") }}>Change Name</button>
+      <br />
+      <br />
+      <h3>My age {Age}</h3>
+      <button onClick={ChangeAge}>Change age</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={Counter}>Count is {Count}</button>
     </div>
+
   );
 }
 
